45467c35d67f4e0579d37dd540a890c10d60da68
옥탑방개발자 <jongwons.choi@gmail.com>
2021-01-06 17:02:42 +0900

    불필요한 단원 삭제

--- server/login-basic/src/main/java/com/sp/fc/web/config/SecurityConfig.java
+++ server/login-basic/src/main/java/com/sp/fc/web/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.sp.fc.web.config;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -17,7 +16,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                             .antMatchers("/**").permitAll()
                             ;
                 })
-//        .formLogin()
                 ;
     }
 }
