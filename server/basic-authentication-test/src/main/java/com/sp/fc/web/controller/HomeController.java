580135e641518d6316c419ac5937101283e8b84c
옥탑방개발자 <jongwons.choi@gmail.com>
2021-01-08 18:21:35 +0900

    Revert "Revert "add""
    This reverts commit 3b7557627b0693d337c637498967fb9b78586223.

---
+++ server/basic-authentication-test/src/main/java/com/sp/fc/web/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.sp.fc.web.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/greeting")
+    public String greeting(){
+        return "hello";
+    }
+
+    @PostMapping("/greeting")
+    public String greeting(@RequestBody  String name){
+        return "hello "+name;
+    }
+
+}
