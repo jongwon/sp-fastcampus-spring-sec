e069527f43902a13e8d3b7b4f173dbc98cf57187
옥탑방개발자 <jongwons.choi@gmail.com>
2021-01-14 16:56:01 +0900

    add

---
+++ server/login-userdetails/src/main/java/com/sp/fc/web/UserDetailsTestApplication.java
@@ -0,0 +1,13 @@
+package com.sp.fc.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDetailsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserDetailsTestApplication.class, args);
+    }
+
+}
