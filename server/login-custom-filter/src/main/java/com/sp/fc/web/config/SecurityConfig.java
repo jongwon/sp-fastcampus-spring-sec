dff96e270842e52bc8678e3c27814003b9c1c2f0
옥탑방개발자 <jongwons.choi@gmail.com>
2021-01-14 13:45:00 +0900

    add

--- server/login-custom-filter/src/main/java/com/sp/fc/web/config/SecurityConfig.java
+++ server/login-custom-filter/src/main/java/com/sp/fc/web/config/SecurityConfig.java
@@ -39,12 +39,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         request.antMatchers("/", "/login").permitAll()
                         .anyRequest().authenticated()
                 )
-//                .formLogin(
-//                        login->login.loginPage("/login")
-//                        .permitAll()
-//                        .defaultSuccessUrl("/", false)
-//                        .failureUrl("/login-error")
-//                )
+                .formLogin(
+                        login->login.loginPage("/login")
+                        .permitAll()
+                        .defaultSuccessUrl("/", false)
+                        .failureUrl("/login-error")
+                )
                 .addFilterAt(filter, UsernamePasswordAuthenticationFilter.class)
                 .logout(logout->logout.logoutSuccessUrl("/"))
                 .exceptionHandling(e->e.accessDeniedPage("/access-denied"))
--- server/login-custom-filter/src/main/java/com/sp/fc/web/student/StudentManager.java
+++ server/login-custom-filter/src/main/java/com/sp/fc/web/student/StudentManager.java
@@ -18,9 +18,9 @@ public class StudentManager implements AuthenticationProvider, InitializingBean 
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) authentication;
-        if(studentDB.containsKey(token.getName())){
-            Student student = studentDB.get(token.getName());
+        StudentAuthenticationToken token = (StudentAuthenticationToken) authentication;
+        if(studentDB.containsKey(token.getCredentials())){
+            Student student = studentDB.get(token.getCredentials());
             return StudentAuthenticationToken.builder()
                     .principal(student)
                     .details(student.getUsername())
@@ -33,7 +33,7 @@ public class StudentManager implements AuthenticationProvider, InitializingBean 
 
     @Override
     public boolean supports(Class<?> authentication) {
-        return authentication == UsernamePasswordAuthenticationToken.class;
+        return authentication == StudentAuthenticationToken.class;
     }
 
     @Override
