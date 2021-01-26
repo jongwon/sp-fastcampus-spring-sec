d62f81a894bbdf845834510f26e4fdf0b4879f8f
옥탑방개발자 <jongwons.choi@gmail.com>
2021-01-26 20:10:55 +0900

    add

--- server/authority-test/src/main/java/com/sp/fc/web/AuthorityTestApplication.java
+++ server/authority-test/src/main/java/com/sp/fc/web/AuthorityTestApplication.java
@@ -1,4 +1,15 @@
 package com.sp.fc.web;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {
+        "com.sp.fc.web"
+})
 public class AuthorityTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorityTestApplication.class, args);
+    }
+
 }
