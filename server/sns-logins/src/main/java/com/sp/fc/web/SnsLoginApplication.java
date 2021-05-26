5c1e4080add1bb7dd803a69a399e5a7d82c4b0f7
jongwon <jongwons.choi@gmail.com>
2021-05-26 16:19:42 +0900

    add

---
+++ server/sns-logins/src/main/java/com/sp/fc/web/SnsLoginApplication.java
@@ -0,0 +1,13 @@
+package com.sp.fc.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnsLoginApplication.class, args);
+    }
+
+}
