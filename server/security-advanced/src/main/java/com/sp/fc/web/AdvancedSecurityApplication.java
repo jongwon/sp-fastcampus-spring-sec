ac9235e84f8733d86553772607c78bdd33a6fd2d
jongwon <jongwons.choi@gmail.com>
2021-05-26 12:15:44 +0900

    add

---
+++ server/security-advanced/src/main/java/com/sp/fc/web/AdvancedSecurityApplication.java
@@ -0,0 +1,16 @@
+package com.sp.fc.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {
+//        "com.sp.fc.config",
+        "com.sp.fc.web"
+})
+public class AdvancedSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvancedSecurityApplication.class, args);
+    }
+
+}
---
+++ server/security-advanced/src/test/java/com/sp/fc/web/JWTSimpleTest.java
@@ -0,0 +1,24 @@
+package com.sp.fc.web;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class JWTSimpleTest {
+
+
+    @DisplayName("1. jjwt 를 이용한 토큰 테스트")
+    @Test
+    void test_1(){
+
+    }
+
+
+    @DisplayName("2. java-jwt 를 이용한 토큰 테스트")
+    @Test
+    void test_2() {
+
+    }
+
+
+
+}
